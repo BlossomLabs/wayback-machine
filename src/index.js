8fa1adeabdf673c805190a6496b523c9b2d3d27d
Sem <931684+sembrestels@users.noreply.github.com>
2022-11-18 18:38:06 -0600

    Include chakra-ui

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createHashRouter, RouterProvider } from 'react-router-dom';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import Root from './routes/root';
 import PageViewer from './routes/page-viewer';
 
@@ -16,8 +17,27 @@ const router = createHashRouter([
   }
 ]);
 
+const colors = {
+  brand: {
+    "50": "#E6FBFE",
+    "100": "#BAF5FC",
+    "200": "#8EEFFB",
+    "300": "#62E8F9",
+    "400": "#36E2F7",
+    "500": "#0ADBF5",
+    "600": "#08AFC4",
+    "700": "#068493",
+    "800": "#045862",
+    "900": "#022C31"
+  },
+}
+
+const theme = extendTheme({ colors })
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <ChakraProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ChakraProvider>
   </React.StrictMode>
 );
--- src/routes/page-viewer.jsx
+++ src/routes/page-viewer.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export async function loader({ params }) {
     console.log(params)

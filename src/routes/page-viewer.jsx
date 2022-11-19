352691573abedd16945e0fe08016228ac3f2fc0a
Sem <931684+sembrestels@users.noreply.github.com>
2022-11-18 19:41:46 -0600

    Retreive IPFS from ENS

--- src/routes/page-viewer.jsx
+++ src/routes/page-viewer.jsx
@@ -1,18 +1,48 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
-export async function loader({ params }) {
-    console.log(params)
-  return params.url;
-}
+import contentHash from 'content-hash'
 
 export default function PageViewer() {
-  
-//   const url = useLoaderData()
+
   const { url } = useParams()
+  const [ipfsUrl, setIpfsUrl] = useState()
+    useEffect(() => {
+        (async () => {
+            const encoded = await fetch('https://api.thegraph.com/subgraphs/name/ensdomains/ens', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    query: `
+                        query Get($ens: String!) {
+                            domains(where: {name: $ens}) {
+                                name
+                                resolver {
+                                    id
+                                    contentHash
+                                }
+                            }
+                        }
+                    `,
+                    variables: {
+                        ens: url
+                    },
+                }),
+            })
+            .then((res) => res.json())
+            .then((result) => result.data.domains[0].resolver.contentHash);
+            const ipfsv0 = contentHash.decode(encoded)
+            const ipfsv1 = contentHash.helpers.cidV0ToV1Base32(ipfsv0)
+            console.log(encoded, ipfsv0, ipfsv1)
+            setIpfsUrl(ipfsv1)
+        })()
+    }, [url])
+
   return (
     <>
     <div>Hola {url}</div>
-    <iframe width="100%" style={{minHeight: "100vh", border: 0}} src={"https://bafybeidxxpqshttat7xxktstmw7bwymuijzhtestounqkqghhghagp6mda.ipfs.dweb.link/"} />
+    <iframe width="100%" style={{minHeight: "100vh", border: 0}} src={ipfsUrl? `https://${ipfsUrl}.ipfs.dweb.link/`: ''} />
     </>
   );
 }

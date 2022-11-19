3da29de881daf93c648f1bd493b689eb6b6de724
Sem <931684+sembrestels@users.noreply.github.com>
2022-11-18 20:18:06 -0600

    Retreive all historic versions of an ENS website

--- src/routes/page-viewer.jsx
+++ src/routes/page-viewer.jsx
@@ -2,40 +2,70 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import contentHash from 'content-hash'
 
+function getFromENSGraph(query, variables, path) {
+    return fetch('https://api.thegraph.com/subgraphs/name/ensdomains/ens', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query,
+            variables,
+        }),
+    })
+    .then((res) => res.json())
+    .then(path);
+    
+}
+
+function decode(encoded) {
+    if (!encoded.startsWith('0xe3')) {
+        // Not IPFS
+        return ''
+    }
+    console.log(encoded)
+    const ipfsv0 = contentHash.decode(encoded)
+    const ipfsv1 = contentHash.helpers.cidV0ToV1Base32(ipfsv0)
+    console.log(encoded, ipfsv0, ipfsv1)
+    return ipfsv1
+}
+
 export default function PageViewer() {
 
   const { url } = useParams()
   const [ipfsUrl, setIpfsUrl] = useState()
     useEffect(() => {
-        (async () => {
-            const encoded = await fetch('https://api.thegraph.com/subgraphs/name/ensdomains/ens', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    query: `
-                        query Get($ens: String!) {
-                            domains(where: {name: $ens}) {
-                                name
-                                resolver {
-                                    id
-                                    contentHash
-                                }
-                            }
+        if (url) (async () => {
+            const resolverId = await getFromENSGraph(`
+                query GetENSResolver($ens: String!) {
+                    domains(where: {name: $ens}) {
+                        name
+                        resolver {
+                            id
                         }
-                    `,
-                    variables: {
-                        ens: url
-                    },
-                }),
-            })
-            .then((res) => res.json())
-            .then((result) => result.data.domains[0].resolver.contentHash);
-            const ipfsv0 = contentHash.decode(encoded)
-            const ipfsv1 = contentHash.helpers.cidV0ToV1Base32(ipfsv0)
-            console.log(encoded, ipfsv0, ipfsv1)
-            setIpfsUrl(ipfsv1)
+                    }
+                }
+            `,
+            { ens: url },
+            (result) => result.data.domains[0].resolver.id
+            )
+            const encoded = await getFromENSGraph(`
+                query GetENSContentHashes($resolverId: String!) {
+                    contenthashChangeds(where: {resolver: $resolverId}) {
+                        blockNumber
+                        hash
+                    }
+                }
+            `,
+            { resolverId },
+            (result) => result.data.contenthashChangeds
+            )
+            const decoded = encoded.map(({hash, blockNumber}) => ({
+                hash: decode(hash),
+                blockNumber
+            }))
+            
+            setIpfsUrl(decoded[decoded.length - 1].hash)
         })()
     }, [url])
 
